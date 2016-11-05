a672daff1320b5a23d8ae8a0fb226322324f6266
andrey <andrey@student_jobofferusa.com>
2016-11-05 13:30:48 -0400

    deploy

--- core/form.py
+++ core/form.py
@@ -2,8 +2,8 @@ from django import forms
 
 
 class RequestEstimateForm(forms.Form):
-    phone = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Your phone'}))
-    email = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Your email'}))
+    phone = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Your phone'}), max_length=10)
+    email = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Your email'}), max_length=30)
     address = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Your address'}))
     message = forms.CharField(widget=forms.Textarea(attrs={'placeholder': 'Your message', 'rows': 4}))
 
@@ -11,7 +11,7 @@ class RequestEstimateForm(forms.Form):
 class ContactUsForm(forms.Form):
     subject = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Subject'}))
     name = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Name'}))
-    phone = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Phone'}))
-    email = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Email'}))
+    phone = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Phone'}), max_length=10)
+    email = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Email'}), max_length=30)
     address = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Address'}))
     message = forms.CharField(widget=forms.Textarea(attrs={'placeholder': 'Message', 'rows': 4}))
