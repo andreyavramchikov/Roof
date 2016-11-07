af47602ac63d81004b39b1866d9871f2c03867bb
andrey <andrey@student_jobofferusa.com>
2016-11-07 14:19:45 -0500

    wer

--- roof/settings.py
+++ roof/settings.py
@@ -128,8 +128,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'aldrson@gmail.com'
-EMAIL_HOST_PASSWORD = 'andreypasan'
+EMAIL_HOST_USER = 'solutioncontractor3@gmail.com'
+EMAIL_HOST_PASSWORD = 'michelle0326'
 
 # michelle0326
 
