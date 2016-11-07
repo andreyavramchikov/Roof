ecba2f4306ece47927d94794ccc1e160edf6918f
andrey <andrey@student_jobofferusa.com>
2016-11-07 14:28:35 -0500

    wer

--- roof/settings.py
+++ roof/settings.py
@@ -129,7 +129,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'solutioncontractor3@gmail.com'
-EMAIL_HOST_PASSWORD = 'andreyPasan1'
+EMAIL_HOST_PASSWORD = 'michelle0326'
 
 # michelle0326
 
